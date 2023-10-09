1d9080a9d8b0dfd2a8d41e2a836a398acfc4bce9
user <user@users-MacBook-Pro.local>
2023-10-09 23:17:52 +0300

    add photo and minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,8 @@ class MyApp extends StatelessWidget {
             icon: const Icon(Icons.menu),
             onPressed: () {},
             color: Colors.black,
-          ), 
-          actions: [
+          ),
+          actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.settings),
               onPressed: () {},
@@ -33,7 +33,61 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
+        body: _buildBody(),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _weatherDescription(),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return const Image(
+    image: NetworkImage(
+        'https://images.squarespace-cdn.com/content/v1/57825361440243db4a4b7830/a6b48724-dc61-4056-8566-ad31d4fcaf61/Gorpcore-UK-gorpgirls-advancedrock-114index-moodboards-gorp-goretex-outdoors-arcteryx-columbia-climbing-hike-instagram-communities-corecore-ukoutdoors-lewiswilson-hannahdasilva-liamfurneaux-sabukaru-online-magazine-tokyo13.jpg'),
+    fit: BoxFit.cover,
+  );
+}
+
+//TODO: Create dynamic weather description, temperature, and location
+
+Column _weatherDescription() {
+  return const Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'Monday - October 9',
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.w200,
+          color: Colors.black,
+        ),
+      ),
+      //Розділитель між текстом
+      Divider(),
+      Text(
+        "In the mountains, at the heart of gorpcore style, our team embarked on a mountain biking race in fair weather, dressed in reliable mountain jackets and retro baseball caps, ready for incredible adventures.",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          color: Colors.blueGrey,
+        ),
+      ),
+    ],
+  );
 }
